18147446aee72fdf94fc065050e260c132c1a578
Mohackz <mohackz@live.fr>
2020-08-04 13:05:31 +0300

    client - implemented map view with react leaftlet

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './sidebar/Sidebar';
-import Map from './map/Map';
+import MapLeaflet from './map/Map';
 
 class App extends React.Component {
   render(){
     return (
       <React.Fragment>
         <Sidebar />
-        <Map />
+        <MapLeaflet />
       </React.Fragment>
       
     );
--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
+import { Map as Leaflet, TileLayer } from 'react-leaflet'
 
-class Map extends Component {
+class MapLeaflet extends Component {
 
   render(){
-
+    const position = [11.572076, 43.145645];
     return (
-      <div id="map" className="map">
-        <h1>Hello World</h1>
-      </div>
+      <Leaflet id="map" center={position} zoom={13}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        />
+      </Leaflet>
     );
 
   }
 
 }
 
-export default Map;
+export default MapLeaflet;
